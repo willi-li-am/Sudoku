e76cf9c0a673fa5a759a83506a211a4d0803e896
William Li <li.william.mail@gmail.com>
2022-01-01 17:31:23 -0500

    fini l'interface utilisateur du 16x16

---
+++ script.js
@@ -0,0 +1,27 @@
+//https://stackoverflow.com/questions/6840326/how-can-i-create-and-style-a-div-using-javascript/6840399
+
+/*function createElements(numElements){
+    var arr = [];
+    for(let i = 0; i < numElements; i++){
+        var div = document.createElement("div");
+        div.style.width = "100px";
+        div.style.height = "100px";
+        div.style.background = "red";
+        div.style.color = "white";
+        div.innerHTML = i;
+        arr.push(div);
+    }
+    return arr;
+}
+
+let arr = createElements(10);
+
+console.log(arr);
+
+function addElementsToPage(arr){
+    for (let i = 0; i < arr.length; i++) {
+        document.getElementById('main').appendChild(arr[i]);
+    }
+}
+
+addElementsToPage(arr)*/
